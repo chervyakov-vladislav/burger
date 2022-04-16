82a5f46a19d5d803677ea7e2397c624fa6f61398
Vladislav Chervyakov <chervyakovve@mail.ru>
2022-04-16 23:07:22 +0700

    add js close btn

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
 // hamburger menu header--fullscreen
 
-var header = document.querySelector('.header');
+const header = document.querySelector('.header');
+const body = document.querySelector('body');
 
 header.addEventListener('click', function(e) {
-	var hamburger = e.target.closest('.hamburger-menu');
+	const hamburger = e.target.closest('.hamburger-menu');
 	if	(hamburger && e.target.closest('.header--fullscreen')) {
 		header.classList.remove('header--fullscreen');
+		body.style.overflow = 'inherit';
 	}
 	else if (hamburger && e.target.closest('.hamburger-menu')) {
 		header.classList.add('header--fullscreen');
+		body.style.overflow = 'hidden';
 	}
 });
+
+
+//burgers-composition-close-btn
+const hoverClose = document.querySelector('.composition__close-btn');
+const hoverOpen = document.querySelector('.burger-card__composition--item');
+
+	hoverClose.addEventListener('click', function(e) {
+		e.preventDefault();
+		document.querySelector('.burger-card__composition--hover').style.display = 'none';
+	});
+	
+if (screen.width < 768) {
+	hoverOpen.addEventListener('click', function(e) {
+		e.preventDefault();
+		document.querySelector('.burger-card__composition--hover').style.display = 'block';
+	});
+}
