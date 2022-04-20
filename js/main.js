2a967c7e0e13ea0def04a279f0ecd5c12410c03e
Vladislav Chervyakov <chervyakovve@mail.ru>
2022-04-20 12:13:58 +0700

    add popup js code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var i;
+
 // hamburger menu header--fullscreen
 
 const header = document.querySelector('.header');
@@ -30,4 +32,43 @@ if (screen.width < 769) {
 		e.preventDefault();
 		document.querySelector('.burger-card__composition--hover').style.display = 'block';
 	});
+}
+
+//overlay reviews
+
+const reviewButton = document.querySelectorAll('.reviews__btn');
+
+for (i = 0; i < reviewButton.length; i++) {
+	reviewButton[i].addEventListener("click", function(e) {
+			let reviewsTitle = e.path[2].querySelector('.reviews__title').innerHTML;
+			let reviewsText = e.path[2].querySelector('.reviews__text').innerHTML;
+		  let successOverlay = createOverlay(reviewsTitle, reviewsText);
+			document.querySelector(".reviews").append(successOverlay);
+    });
+}
+
+
+function createOverlay(title, text) {
+	//не получилось добавить готовый шаблон на страницу, пришлось создавать див с оверлеем (newElement = document.createElement('div');) и в него закидывать шиблон
+	const newElement = document.createElement('div');
+	newElement.classList.add('overlay');
+	newElement.innerHTML = document.querySelector('#reviewsPopup').innerHTML;
+	
+	const closeOverlay = newElement.querySelector('.overlay__close-btn');
+	const closeOverlayBg = newElement.querySelector('.overlay');
+
+	closeOverlayBg.addEventListener('click', function(e) {
+		if (e.target === closeOverlayBg) {
+			closeOverlay.click();
+		}
+	});
+	closeOverlay.addEventListener('click', function(e) {
+		e.preventDefault();
+		document.querySelector('.reviews').removeChild(newElement);
+	})
+
+	newElement.querySelector('.reviews__overlay--title').innerHTML = title;
+	newElement.querySelector('.reviews__overlay--text').innerHTML = text;
+
+	return newElement;
 }
