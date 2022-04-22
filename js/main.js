c43e1728a5dea1c846b4b054ddcf33aa10d14a9b
Vladislav Chervyakov <chervyakovve@mail.ru>
2022-04-22 21:13:49 +0700

    add js slider

--- js/main.js
+++ js/main.js
@@ -73,9 +73,9 @@ function createOverlay(title, text) {
 //accordeon
 
 const itemVertAccordeon = document.querySelectorAll('.vertical-accordeon__item');
-//не работает preventDefault;
 for (i = 0; i < itemVertAccordeon.length; i++) {
 	itemVertAccordeon[i].addEventListener('click', function(e) {
+		e.preventDefault();
 		if (e.target.closest('.vertical-accordeon__item').classList.contains('vertical-accordeon__item--active')) {
 			e.target.closest('.vertical-accordeon__item').classList.remove('vertical-accordeon__item--active');
 		} else {
@@ -102,4 +102,47 @@ for (i = 0; i < itemTeamAccordeon.length; i++) {
 	});
 }
 
-// slider menu
+// slider
+// не сделаны буллеты снизу
+sliderContainer = document.querySelector('.slider-container__list');
+sliderCarousel = document.querySelector('.slider-container__items');
+sliderItem = document.querySelectorAll('.slider-container__item');
+contentWidth = document.querySelector('.slider-container__list').clientWidth;
+sliderLeft = document.querySelector('#slider-left');
+sliderRight = document.querySelector('#slider-right');
+
+const minRight = 0;
+const maxRight = contentWidth * sliderItem.length;
+const step = contentWidth;
+let currentRight = 0;
+
+// сам догадался
+for (i=0; i < sliderItem.length; i++) {
+	sliderItem[i].style.minWidth = contentWidth + "px";
+}
+//подглядел как надо
+// sliderItem.forEach(item => {
+// 	item.style.minWidth = `${contentWidth}px`;
+// });
+
+sliderLeft.addEventListener('click', e => {
+	e.preventDefault();
+  if (Math.abs(currentRight) > minRight) {
+    currentRight += step;
+    sliderCarousel.style.transform = `translateX(${currentRight}px)`;
+  } else {
+		sliderCarousel.style.transform = `translateX(-${maxRight-step}px)`;
+		currentRight = -(maxRight-step);
+	}
+});
+
+sliderRight.addEventListener('click', e => {
+	e.preventDefault();
+	if (Math.abs(currentRight) < (maxRight-step)) {
+    currentRight -= step;
+    sliderCarousel.style.transform = `translateX(${currentRight}px)`;
+  } else {
+		sliderCarousel.style.transform = `translateX(0px)`;
+		currentRight = 0;
+	}
+});
