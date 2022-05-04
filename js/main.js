d8bea11de642d7fa977935b357b57f221e17236a
Vladislav Chervyakov <chervyakovve@mail.ru>
2022-05-04 11:45:45 +0700

    uslss commit

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ var i;
 const header = document.querySelector('.header');
 const body = document.querySelector('body');
 
-header.addEventListener('click', function(e) {
+header.addEventListener('click', function (e) {
 	const hamburger = e.target.closest('.hamburger-menu');
-	if	(hamburger && e.target.closest('.header--fullscreen')) {
+	if (hamburger && e.target.closest('.header--fullscreen')) {
 		header.classList.remove('header--fullscreen');
 		body.style.overflow = 'inherit';
 	}
@@ -21,13 +21,13 @@ header.addEventListener('click', function(e) {
 const hoverClose = document.querySelector('.composition__close-btn');
 const hoverOpen = document.querySelector('.burger-card__composition--item');
 
-	hoverClose.addEventListener('click', function(e) {
-		e.preventDefault();
-		document.querySelector('.burger-card__composition--hover').style.display = 'none';
-	});
-	
+hoverClose.addEventListener('click', function (e) {
+	e.preventDefault();
+	document.querySelector('.burger-card__composition--hover').style.display = 'none';
+});
+
 if (screen.width < 769) {
-	hoverOpen.addEventListener('click', function(e) {
+	hoverOpen.addEventListener('click', function (e) {
 		e.preventDefault();
 		document.querySelector('.burger-card__composition--hover').style.display = 'block';
 	});
@@ -38,12 +38,12 @@ const reviewButton = document.querySelectorAll('.reviews__btn');
 const template = document.querySelector('#reviewsPopup').innerHTML;
 
 for (i = 0; i < reviewButton.length; i++) {
-	reviewButton[i].addEventListener("click", function(e) {
-			let reviewsTitle = e.path[2].querySelector('.reviews__title').innerHTML;
-			let reviewsText = e.path[2].querySelector('.reviews__text').innerHTML;
-		  let successOverlay = createOverlay(reviewsTitle, reviewsText);
-			document.querySelector(".reviews").append(successOverlay);
-    });
+	reviewButton[i].addEventListener("click", function (e) {
+		let reviewsTitle = e.path[2].querySelector('.reviews__title').innerHTML;
+		let reviewsText = e.path[2].querySelector('.reviews__text').innerHTML;
+		let successOverlay = createOverlay(reviewsTitle, reviewsText);
+		document.querySelector(".reviews").append(successOverlay);
+	});
 }
 
 function createOverlay(title, text) {
@@ -54,12 +54,12 @@ function createOverlay(title, text) {
 	const closeOverlay = newElement.querySelector('.overlay__close-btn');
 	const closeOverlayBg = newElement.querySelector('.overlay');
 
-	closeOverlayBg.addEventListener('click', function(e) {
+	closeOverlayBg.addEventListener('click', function (e) {
 		if (e.target === closeOverlayBg) {
 			closeOverlay.click();
 		}
 	});
-	closeOverlay.addEventListener('click', function(e) {
+	closeOverlay.addEventListener('click', function (e) {
 		e.preventDefault();
 		document.querySelector('.reviews').removeChild(newElement);
 	})
@@ -74,7 +74,7 @@ function createOverlay(title, text) {
 
 const itemVertAccordeon = document.querySelectorAll('.vertical-accordeon__item');
 for (i = 0; i < itemVertAccordeon.length; i++) {
-	itemVertAccordeon[i].addEventListener('click', function(e) {
+	itemVertAccordeon[i].addEventListener('click', function (e) {
 		e.preventDefault();
 		if (e.target.closest('.vertical-accordeon__item').classList.contains('vertical-accordeon__item--active')) {
 			e.target.closest('.vertical-accordeon__item').classList.remove('vertical-accordeon__item--active');
@@ -83,14 +83,14 @@ for (i = 0; i < itemVertAccordeon.length; i++) {
 				itemVertAccordeon[j].classList.remove('vertical-accordeon__item--active');
 			}
 			e.target.closest('.vertical-accordeon__item').classList.add('vertical-accordeon__item--active');
-		}		
+		}
 	});
 }
 
 //team-accordeon
 const itemTeamAccordeon = document.querySelectorAll('.staff__item');
 for (i = 0; i < itemTeamAccordeon.length; i++) {
-	itemTeamAccordeon[i].addEventListener('click', function(e) {
+	itemTeamAccordeon[i].addEventListener('click', function (e) {
 		if (e.target.closest('.staff__item').classList.contains('staff__active')) {
 			e.target.closest('.staff__item').classList.remove('staff__active');
 		} else {
@@ -116,27 +116,27 @@ const maxRight = contentWidth * sliderItem.length;
 const step = contentWidth;
 let currentRight = 0;
 
-for (i=0; i < sliderItem.length; i++) {
+for (i = 0; i < sliderItem.length; i++) {
 	sliderItem[i].style.minWidth = contentWidth + "px";
 }
 
 sliderLeft.addEventListener('click', e => {
 	e.preventDefault();
-  if (Math.abs(currentRight) > minRight) {
-    currentRight += step;
-    sliderCarousel.style.transform = `translateX(${currentRight}px)`;
-  } else {
-		sliderCarousel.style.transform = `translateX(-${maxRight-step}px)`;
-		currentRight = -(maxRight-step);
+	if (Math.abs(currentRight) > minRight) {
+		currentRight += step;
+		sliderCarousel.style.transform = `translateX(${currentRight}px)`;
+	} else {
+		sliderCarousel.style.transform = `translateX(-${maxRight - step}px)`;
+		currentRight = -(maxRight - step);
 	}
 });
 
 sliderRight.addEventListener('click', e => {
 	e.preventDefault();
-	if (Math.abs(currentRight) < (maxRight-step)) {
-    currentRight -= step;
-    sliderCarousel.style.transform = `translateX(${currentRight}px)`;
-  } else {
+	if (Math.abs(currentRight) < (maxRight - step)) {
+		currentRight -= step;
+		sliderCarousel.style.transform = `translateX(${currentRight}px)`;
+	} else {
 		sliderCarousel.style.transform = `translateX(0px)`;
 		currentRight = 0;
 	}
@@ -144,7 +144,7 @@ sliderRight.addEventListener('click', e => {
 
 //form popup вытаскиваем шаблон попапа и закидываем в форму
 const templateForm = document.querySelector('#formPopup').innerHTML;
-const sendButton =  document.querySelector('#sendBtn');
+const sendButton = document.querySelector('#sendBtn');
 const sendForm = document.querySelector('.form__elem');
 
 let dataForm;
@@ -163,7 +163,7 @@ sendButton.addEventListener('click', (e) => {
 		xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail')
 		xhr.responseType = 'json';
 		xhr.send(formData);
-		xhr.addEventListener('load', function() {
+		xhr.addEventListener('load', function () {
 			if (xhr.response.status === 1) {
 				textFormPopup = xhr.response.message;
 				createFormOverlay(textFormPopup);
@@ -183,12 +183,12 @@ function createFormOverlay(text) {
 	const closeOverlay = newElement.querySelector('.btn--overlay');
 	const closeOverlayBg = newElement.querySelector('.overlay');
 
-	closeOverlayBg.addEventListener('click', function(e) {
+	closeOverlayBg.addEventListener('click', function (e) {
 		if (e.target === closeOverlayBg) {
 			closeOverlay.click();
 		}
 	});
-	closeOverlay.addEventListener('click', function(e) {
+	closeOverlay.addEventListener('click', function (e) {
 		e.preventDefault();
 		document.querySelector('.order').removeChild(newElement);
 	})
@@ -198,7 +198,7 @@ function createFormOverlay(text) {
 	document.querySelector(".order").append(newElement);
 }
 
-function validateForm(form){
+function validateForm(form) {
 	let valid = true;
 
 	if (!validateField(form.elements.comment)) {
@@ -221,15 +221,15 @@ function validateForm(form){
 
 function validateField(field) {
 	return field.checkValidity();
-} 
+}
 
 function createData() {
 	let formData = new FormData(sendForm);
 
-        formData.append("name", sendForm.elements.name.value);
-        formData.append("phone", sendForm.elements.phone.value);
-        formData.append("comment", sendForm.elements.comment.value);
-        formData.append("to", "vasy@liy.com");
+	formData.append("name", sendForm.elements.name.value);
+	formData.append("phone", sendForm.elements.phone.value);
+	formData.append("comment", sendForm.elements.comment.value);
+	formData.append("to", "vasy@liy.com");
 
 	return formData;
 	// почему не сработал stringify?
