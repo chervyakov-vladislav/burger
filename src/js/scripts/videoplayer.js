08673691be7ec9c766abc8b5f1d77172b857a435
Vladislav Chervyakov <chervyakovve@mail.ru>
2022-05-14 18:07:10 +0700

    add videocontrols

--- src/js/scripts/videoplayer.js
+++ src/js/scripts/videoplayer.js
@@ -3,6 +3,15 @@
 	let tag = document.createElement('script');
 	let playButton = document.querySelector('.videocontent__play-btn');
 	let pauseButton = document.querySelector('.videocontent__pause-btn');
+	let playButtonControls = document.querySelector('.controls__play');
+	let pauseButtonControls = document.querySelector('.controls__pause');
+	let hideControls = document.querySelector('.videocontent__controls');
+	let volumeInput = document.querySelector('.controls__volume-level')
+	let volumeButton = document.querySelector('.controls__volume');
+	let currentTimeElem = document.querySelector('.controls__timeline-current');
+	let totalTimeElem = document.querySelector('.controls__timeline-total');
+	let speedButton = document.querySelector('.controls__speed-btn');
+	let timeline = document.querySelector('.controls__timeline');
 
 	tag.src = "https://www.youtube.com/iframe_api";
 	let firstScriptTag = document.getElementsByTagName('script')[0];
@@ -30,34 +39,150 @@
 	}
 
 	onPlayerReady = () => {
-		player.setVolume(50);
-		playButton.addEventListener('click', () => {
-			playButton.classList.add('videocontent__play-btn--active');
-			pauseButton.classList.add('videocontent__pause-btn--active');
-			player.playVideo();
-		});
+		playPauseVideoOnPlayerReady();
+		setTotalTime();
+		setTimelineParams();
 	}
 
 	onPlayerStateChange = () => {
-		if (player.getPlayerState() == 2 || player.getPlayerState() == 0) {
-			playButton.classList.remove('videocontent__play-btn--active');
-			pauseButton.classList.remove('videocontent__pause-btn--active');
-			player.pauseVideo();
+		playPauseVideoOnPlayerStateChange();
+		setVolume();
+		muteVolume();
+		setInterval(setCurrentTime, 500);
+		changeSpeed();
+		changeTime();
+	}
+
+	playPauseVideoOnPlayerReady = () => {
+		playButton.addEventListener('click', togglePlay);
+		playButtonControls.addEventListener('click', togglePlay);
+	}
+
+
+	playPauseVideoOnPlayerStateChange = () => {
+		if (player.getPlayerState() == 2) {
+			togglePause();
 		}
+
+		if (player.getPlayerState() == 0) {
+			toggleStop();
+		}
+
 		pauseButton.addEventListener('click', () => {
-			playButton.classList.remove('videocontent__play-btn--active');
-			pauseButton.classList.remove('videocontent__pause-btn--active');
-			player.pauseVideo();
+			togglePause();
 		});
 
+		pauseButtonControls.addEventListener('click', () => {
+			togglePause();
+		});
+	}
+
+	
+	//play pause
+	togglePlay = () => {
+		playButton.classList.add('videocontent__play-btn--active');
+		pauseButton.classList.add('videocontent__pause-btn--active');
+		pauseButtonControls.classList.add('controls__pause--active');
+		playButtonControls.classList.remove('controls__play--active');
+		hideControls.classList.remove('videocontent__controls--paused');
+		player.playVideo();
+	}
+
+	togglePause = () => {
+		playButton.classList.remove('videocontent__play-btn--active');
+		pauseButton.classList.remove('videocontent__pause-btn--active');
+		pauseButtonControls.classList.remove('controls__pause--active');
+		playButtonControls.classList.add('controls__play--active');
+		hideControls.classList.add('videocontent__controls--paused');
+		player.pauseVideo();
+	}
+
+	toggleStop = () => {
+		playButton.classList.remove('videocontent__play-btn--active');
+		pauseButton.classList.remove('videocontent__pause-btn--active');
+		pauseButtonControls.classList.remove('controls__pause--active');
+		playButtonControls.classList.add('controls__play--active');
+		player.stopVideo();
+	}
+
+	//volume
+	muteVolume = () => {
+		volumeButton.addEventListener('click', () =>{
+			if (player.isMuted()) {
+				player.unMute();
+				volumeButton.classList.remove('controls__volume--muted');
+				player.setVolume(50);
+				volumeInput.value = 50;
+			} else 
+			{
+				player.mute();
+				volumeButton.classList.add('controls__volume--muted');
+			}
+		});
+	}
+
+	setVolume = () => {
+		player.setVolume(volumeInput.value);
+		volumeInput.addEventListener('input', (e) => {
+			player.setVolume(e.target.value);
+			if(e.target.value == 0) {
+				volumeButton.classList.add('controls__volume--muted');
+				player.mute();
+			} else {
+				volumeButton.classList.remove('controls__volume--muted');
+				player.unMute();
+			}
+		})
+	}
+
+	//duration
+	setTotalTime = () => {
+		totalTimeElem.innerHTML = formatDurtion(player.getDuration());
+	}
+
+	setCurrentTime = () => {
+		currentTimeElem.innerHTML = formatDurtion(player.getCurrentTime());
+		timeline.value = player.getCurrentTime();
+	}
+
+	formatDurtion = (time) => {
+		const seconds = Math.floor(time % 60);
+		const minutes = Math.floor(time / 60) % 60 ;
+		if (minutes == 0 && seconds < 10) {
+			return ('0:0'+`${seconds}`);
+		} else if (minutes == 0) {
+			return ('0:'+`${seconds}`);
+		} else {
+			return (`${minutes}`+':'+`${seconds}`);
+		}
+	}
+
+
+	changeSpeed = () => {
+		speedButton.addEventListener('click', changeSpeedValue);
+	}
+
+	changeSpeedValue = () => {
+		if (player.getPlaybackRate() == 1) {
+			player.setPlaybackRate(2);
+			speedButton.innerHTML = "2x";
+		} else {
+			player.setPlaybackRate(1);
+			speedButton.innerHTML = "1x";
+		}
+	}
+
+
+	//timeline
+	setTimelineParams = () => {
+		timeline.setAttribute('min', 0);
+		timeline.setAttribute('max', player.getDuration());
+	}
+
+	changeTime = () => {
+		timeline.addEventListener('input', (e) => {
+			player.seekTo(e.target.value);
+			togglePlay();
+		});
 	}
 })();
-
-
-// getPlayerState() статус видео
-//-1 – воспроизведение видео не началось
-// 0 – воспроизведение видео завершено
-// 1 – воспроизведение
-// 2 – пауза
-// 3 – буферизация
-// 5 – видео находится в очереди
