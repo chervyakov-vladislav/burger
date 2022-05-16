e2161280de5e24c7a195a0123fe2fc66f9b5e4de
Vladislav Chervyakov <chervyakovve@mail.ru>
2022-05-16 22:33:53 +0700

    add scroll to desktop version

---
+++ src/js/scripts/scroll.js
@@ -0,0 +1,101 @@
+;(function() {
+
+	const sections = document.querySelectorAll('.section');
+	const display = document.querySelector('.maincontent');
+	let inscroll = false;
+	let  dataScroll = document.querySelectorAll("[data-scroll-to]");
+
+	const performTransition = sectionIndex => {
+		if (inscroll == false) {
+			inscroll = true;
+			let position = `${sectionIndex * (-100)}%`;
+
+			sections[sectionIndex].classList.add('section--active');
+			let siblings = getSiblings(sections[sectionIndex]);
+			for (let sibling in siblings) {
+				if (siblings[sibling].classList.contains('section--active')) {
+					siblings[sibling].classList.remove('section--active');
+				}
+			}
+
+			display.style.cssText = `transform: translateY(${position});`;
+			setTimeout(() => {
+				inscroll = false;
+			}, 200 + 300);
+		}
+	} 
+
+	const scrollViewport = direction => {
+		
+		let activeSection = 0;
+		for (let i = 0; i < sections.length; i++) {
+			if (sections[i].classList.contains('section--active')) {
+				activeSection = i;
+			}
+		}
+		
+		const prevSection = activeSection - 1;
+		const nextSection = activeSection + 1;
+
+		if (direction === 'prev' && prevSection >= 0) {
+			performTransition(prevSection);
+		}
+
+		if (direction === 'next' && nextSection < sections.length) {
+			performTransition(nextSection);
+		}
+	}
+
+	document.addEventListener('wheel', (e) => {
+		let deltaY = e.deltaY;
+		
+		if (deltaY > 0) {
+			scrollViewport('next')
+		} 
+		
+		if (deltaY < 0) {
+			scrollViewport('prev');
+		} 
+
+	});
+
+
+	function getSiblings(elem) {
+    let siblings = [];
+    let sibling = elem;
+    while (sibling.previousSibling) {
+        sibling = sibling.previousSibling;
+        sibling.nodeType == 1 && siblings.push(sibling);
+    }
+
+    sibling = elem;
+    while (sibling.nextSibling) {
+        sibling = sibling.nextSibling;
+        sibling.nodeType == 1 && siblings.push(sibling);
+    }
+
+    return siblings;
+}
+
+document.addEventListener('keydown', (e) => {
+
+	switch(e.keyCode) {
+		case 38 : 
+			scrollViewport('prev');
+			break;
+		case 40 :
+			scrollViewport('next');
+			break;
+	}
+});
+
+
+for (let i=0; i < dataScroll.length; i++) {
+	dataScroll[i].addEventListener('click', (e) => {
+		e.preventDefault();
+		let target = parseInt(e.target.dataset.scrollTo);
+		performTransition(target);
+	})
+}
+
+})();
